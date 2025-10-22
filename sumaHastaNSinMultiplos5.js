7d5a0a3c4470d051ad6246d3b0a4269ea9739d28
Diana Rocio Ramos Rodrigo <dramosrod@unsa.edu.pe>
2025-10-22 16:25:25 -0500

    VF-ejercicio 7

--- sumaHastaNSinMultiplos5.js
+++ sumaHastaNSinMultiplos5.js
@@ -1,5 +1,6 @@
 //Suma todos los números desde 1 hasta N, pero omite los múltiplos de 5. Programa iterativo
-
+function sumaHastaNSinMultiplos5(){
+    
 let N = parseInt(prompt("Ingresa el número límite N:"), 10);
 
 let suma = 0;
@@ -12,3 +13,5 @@ for (let i = 1; i <= N; i++) {
 }
 
 alert(`La suma de los números del 1 al ${N}, omitiendo los múltiplos de 5, es: ${suma}`);
+
+}
